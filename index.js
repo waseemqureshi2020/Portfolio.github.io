60247ed2de2cd6e6fe47bfae109e74a8141e5ed3
Waseem Qureshi <81438868+waseemqureshi2020@users.noreply.github.com>
2022-10-11 22:32:44 +0500

    Fix Linters Errors
    Fix Linters Errors

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const navMenu = document.querySelector(".navMenu");
-const navLink = document.querySelectorAll(".navLink");
-const hamburger = document.querySelector(".hamburger");
-const closedIcon = document.querySelector(".closedIcon");
-const hamburgerIcon = document.querySelector(".hamburgerIcon");
+const navMenu = document.querySelector('.navMenu');
+const navLink = document.querySelectorAll('.navLink');
+const hamburger = document.querySelector('.hamburger');
+const closedIcon = document.querySelector('.closedIcon');
+const hamburgerIcon = document.querySelector('.hamburgerIcon');
 
 function toggleMenu() {
-  if (navMenu.classList.contains("showMenu")) {
-    navMenu.classList.remove("showMenu");
-    closedIcon.style.display = "none";
-    hamburgerIcon.style.display = "block";
+  if (navMenu.classList.contains('showMenu')) {
+    navMenu.classList.remove('showMenu');
+    closedIcon.style.display = 'none';
+    hamburgerIcon.style.display = 'block';
   } else {
-    navMenu.classList.add("showMenu");
-    closedIcon.style.display = "block";
-    hamburgerIcon.style.display = "none";
+    navMenu.classList.add('showMenu');
+    closedIcon.style.display = 'block';
+    hamburgerIcon.style.display = 'none';
   }
 }
 
-hamburger.addEventListener("click", toggleMenu);
+hamburger.addEventListener('click', toggleMenu);
 
-navLink.forEach(function (navLink) {
-  navLink.addEventListener("click", toggleMenu);
+navLink.forEach(function (navLink => {
+navLink.addEventListener('click', toggleMenu);
 });
