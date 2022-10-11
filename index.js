412cfd787ec12dfcd3f8eb49e5c4e2c447cbc100
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2022-10-11 18:17:10 +0500

    Create menu items using JavaScript and style them using CSS

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
+
+// hamburger.addEventListener("click", () =>; {
+//     hamburger.classList.toggle("active");
+//     navMenu.classList.toggle("active");
+// })
+
+// document.querySelectorAll(".nav-links").forEach(n => n.
+// addEventListener("click", () => {
+//     hamburger.classList.remove("active");
+//     navMenu.classList.remove("active");
+// }))
+
+const navMenu = document.querySelector(".navMenu");
+const navLink = document.querySelectorAll(".navLink");
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const closedIcon = document.querySelector(".closedIcon");
+const hamburgerIcon = document.querySelector(".hamburgerIcon");
 
-hamburger.addEventListener("click", () =>; {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+function toggleMenu() {
+  if (navMenu.classList.contains("showMenu")) {
+    navMenu.classList.remove("showMenu");
+    closedIcon.style.display = "none";
+    hamburgerIcon.style.display = "block";
+  } else {
+    navMenu.classList.add("showMenu");
+    closedIcon.style.display = "block";
+    hamburgerIcon.style.display = "none";
+  }
+}
 
-document.querySelectorAll(".nav-links").forEach(n => n.
-addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
+hamburger.addEventListener("click", toggleMenu);
+
+navLink.forEach(function (navLink) {
+  navLink.addEventListener("click", toggleMenu);
+});
